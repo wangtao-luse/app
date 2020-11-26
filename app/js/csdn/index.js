e2b164b7fea3f186eeace6bb204e4a2ce92bd35a
wwang-luse <wwangtaoc11@163.com>
2020-11-26 13:43:01 +0800

    csdn首页导航

--- app/js/csdn/index.js
+++ app/js/csdn/index.js
@@ -1,32 +1,16 @@
 $(function(){
-	/**logo二级菜单**/
-	$("#toolbar-logo").mouseenter(function(){
-		var logo = $("#csdn-toolbar .toolbar-logo .toolbar-subMenu");
-		  logo.show();
-	});
-	$("#toolbar-logo").mouseleave(function(){
-		var logo = $("#csdn-toolbar .toolbar-logo .toolbar-subMenu");
-		  logo.hide();
-	});
-	 /**head菜单二级菜单**/
-	$("#csdn-toolbar .toolbar-menus li").mouseenter(function(){
-		var hasSubMenu = $(this).find("div").hasClass("toolbar-subMenu");
-		if(hasSubMenu){
-			 $(this).find("div.toolbar-subMenu").show();
-		}
-	});
-	$("#csdn-toolbar .toolbar-menus li").mouseleave(function(){
-		var hasSubMenu = $(this).find("div").hasClass("toolbar-subMenu");
-		if(hasSubMenu){
-			 $(this).find("div.toolbar-subMenu").hide();
-		}
-	});
 	
-	
-	
+	$("#csdn-toolbar .toolbar-subMenu-box").mouseenter(function(){
+		$(this).find(".toolbar-subMenu").show();
+	});
+	$("#csdn-toolbar .toolbar-subMenu-box").mouseleave(function(){
+		$(this).find(".toolbar-subMenu").hide();
+	});
+	//搜索框
 	$("#toolbar-search-input").keyup(function(){
 		$("#csdn-toolbar .toolbar-search .toolbar-search-drop-menu").show();
 		$("#csdn-toolbar .toolbar-menus").css("width","242px");
 		$("#csdn-toolbar .toolbar-menus li:eq(4)~li").hide();
 	});
+	
 })
