d027ac595897e99f930421f50424501c1c643d80
wwang-luse <wwangtaoc11@163.com>
2020-12-02 10:27:35 +0800

    init

---
+++ app/js/itour/index.js
@@ -0,0 +1,8 @@
+$(function(){
+		$("#itour-banner .toolbar-subMenu-box").mouseenter(function(){
+		$(this).find(".toolbar-subMenu").show();
+	});
+	$("#itour-banner .toolbar-subMenu-box").mouseleave(function(){
+		$(this).find(".toolbar-subMenu").hide();
+	});
+})
