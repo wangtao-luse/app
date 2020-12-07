176667cb94201c25c1b19794966d418dcff000cd
wangtao-luse <wwangtaoc11@163.com>
2020-12-07 19:03:58 +0800

    init

--- app/js/itour/index.js
+++ app/js/itour/index.js
@@ -5,4 +5,14 @@ $(function(){
 	$("#itour-banner .toolbar-subMenu-box").mouseleave(function(){
 		$(this).find(".toolbar-subMenu").hide();
 	});
+	$("article .contentItem-action.button").mouseenter(function(){
+		$(this).find(".isdefault").css("display","none");
+		$(this).find(".isactive").css("display","block");
+		$(this).css("color","#0077E6");
+	});
+	$("article .contentItem-action.button").mouseleave(function(){
+		$(this).find(".isdefault").css("display","block");
+		$(this).css("color","#646464");
+		$(this).find(".isactive").css("display","none");
+	});
 })
